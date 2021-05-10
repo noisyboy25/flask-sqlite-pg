5632152ae982a734bdcb587ea2293a5cf2989dcb
noisyboy25 <dj402and@gmail.com>
2021-05-10 16:20:17 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, send_from_directory
+from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
+def hello_world():
+    return send_from_directory('public', 'index.html')
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+class Test(Resource):
+    def get(self):
+        return parser.parse_args()
+
+
+api.add_resource(HelloWorld, '/api/helloworld')
+api.add_resource(Test, '/api/test')
